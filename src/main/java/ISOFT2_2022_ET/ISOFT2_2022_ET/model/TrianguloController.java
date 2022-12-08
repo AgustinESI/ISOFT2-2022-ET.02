08d655d66e94cbef8ec5347ebfc1597c40cc5aa3
Agustin Sanchez <Agustin.Sanchez9@alu.uclm.es>
2022-12-08 17:35:09 +0100

    Añadida comprobacion de triangulo

--- src/main/java/ISOFT2_2022_ET/ISOFT2_2022_ET/model/TrianguloController.java
+++ src/main/java/ISOFT2_2022_ET/ISOFT2_2022_ET/model/TrianguloController.java
@@ -3,6 +3,15 @@ package ISOFT2_2022_ET.ISOFT2_2022_ET.model;
 public class TrianguloController {
 
 	public static void tipoDeTriangulo(Triangulo t) {
+
+		if ((t.getAngulo1() + t.getAngulo2() + t.getAngulo3()) > 180) {
+			throw new RuntimeException("Angulos no validos");
+		}
+
+		if (Math.pow(t.getAngulo3(), 2) != (Math.pow(t.getAngulo1(), 2) + Math.pow(t.getAngulo2(), 2))) {
+			throw new RuntimeException("Lados no validos");
+		}
+
 		// Ifs para comprobar que tipo es segun los lados
 		if (t.getLado1() == t.getLado2() && t.getLado1() == t.getLado3()) {
 			System.out.println("Triangulo Equilatero.");
